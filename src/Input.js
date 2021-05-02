74092c7ad1bd6fd2e5912a66e3c8242e7abbcd9b
Marko221285 <horvat.marko2212@gmail.com>
2021-05-02 18:26:18 +0200

    Initial commit

---
+++ src/Input.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Input () {
+
+    const [newText, setnewText] = React.useState('');
+
+    const changeText = (e) => {
+        setnewText(e.target.value);
+    }
+
+    return (
+        <div>
+            <form>
+                <input type='text'
+                       value={newText}
+                       onChange={changeText} />
+                <input type='submit' 
+                       value='Send' />
+            </form>
+        </div>
+    )
+
+}
